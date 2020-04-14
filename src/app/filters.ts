43e9e08a1957041f58ed5c5f7032140846840824
Marcel Koonstra <marcel.koonstra@wijnumbrella.nl>
2020-04-14 14:29:09 +0200

    Shorten creators guids in the address

--- src/app/filters.ts
+++ src/app/filters.ts
@@ -1,3 +1,8 @@
+import { stringify } from 'querystring';
+import * as short from "short-uuid";
+
+let translator = short();
+
 export interface Filters {
     repositories: string[];
     creators: string[];
@@ -8,11 +13,11 @@ export function encodeFilter(filter: Filters): string {
     var searchParams = [];
 
     if (filter.creators && filter.creators.length > 0) {
-      searchParams.push('creators=' + filter.creators.map(encodeURIComponent).join(';'));
+      searchParams.push('creators=' + filter.creators.map(encodeGuid).join(';'));
     }
 
     if (filter.repositories && filter.repositories.length > 0) {
-      searchParams.push('repositories=' + filter.repositories.map(encodeURIComponent).join(';'));
+      searchParams.push('repositories=' + filter.repositories.join(';'));
     }
 
     if (filter.showDrafts) {
@@ -22,11 +27,22 @@ export function encodeFilter(filter: Filters): string {
     return searchParams.join('&');
 }
 
+export function encodeGuid(input: string): string {
+  return translator.fromUUID(input);
+}
+
+export function decodeGuid(input: string): string {
+  if (input.length >= 32) // Fallback for old guids saved in bookmarks
+    return input;
+
+  return translator.toUUID(input);
+}
+
 export function decodeFilter(searchParams: string): Filters {
     var s = new URLSearchParams(searchParams);
 
     return {
-        creators: s.has('creators') ? s.get('creators').split(';') : [],
+        creators: s.has('creators') ? s.get('creators').split(';').map(decodeGuid) : [],
         repositories: s.has('repositories') ? s.get('repositories').split(';') : [],
         showDrafts: s.has('showDrafts') ? s.get('showDrafts') == '1' : false
     }
